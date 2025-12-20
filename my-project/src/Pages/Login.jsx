578fcd2ffd9d043b2cb6aae08d4e764af129ff26
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-20 22:37:08 +0600

    register form work is done

--- my-project/src/Pages/Login.jsx
+++ my-project/src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className='w-3/6 mx-auto bg-[#FFFFFF] mt-10 p-10'>
+        <div className='w-full md:w-3/6 mx-auto bg-[#FFFFFF] mt-10 p-10'>
             <h1 className='font-semibold text-center my-5 text-xl'>Login your account</h1>
             <div className='w-3/6 mx-auto'>
                 <fieldset className="fieldset">
@@ -16,7 +16,7 @@ const Login = () => {
      
                 </fieldset>
 
-               <button className="btn bg-[#403F3F] font-semibold text-white w-full mt-5">Login</button>
+               <button type='submit' className="btn bg-[#403F3F] font-semibold text-white w-full mt-5">Login</button>
 
                <p className='mt-5'>Dont’t Have An Account ? <Link to={'/auth/register'} className='text-red-300'>Register</Link></p>
                 
--- my-project/src/Pages/Register.jsx
+++ my-project/src/Pages/Register.jsx
@@ -3,9 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div className='w-3/6 mx-auto bg-[#FFFFFF] mt-10 p-10'>
+        <div className='w-full md:w-3/6 mx-auto bg-[#FFFFFF] mt-10 p-5'>
             <h1 className='font-semibold text-center my-5 text-xl'>Register your account</h1>
             <div className='w-3/6 mx-auto'>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Name</legend>
+                    <input type="text" className="input" placeholder="Type your Name" />
+                </fieldset>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Photo Url</legend>
+                    <input type="text" className="input" placeholder="Type your photo url" />
+                </fieldset>
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend">Email</legend>
                     <input type="email" className="input" placeholder="Type your email" />
@@ -16,7 +24,7 @@ const Register = () => {
 
                 </fieldset>
 
-                <button className="btn bg-[#403F3F] font-semibold text-white w-full mt-5">Login</button>
+                <button type='submit' className="btn bg-[#403F3F] font-semibold text-white w-full mt-5">Login</button>
 
                 <p className='mt-5'>Dont’t Have An Account ? <Link to={'/auth/login'} className='text-red-300'>Login</Link></p>
 
