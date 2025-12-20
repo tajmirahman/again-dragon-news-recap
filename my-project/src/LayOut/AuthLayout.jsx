089e9955bff2b5e8fc9296e0eb527fa9ca27db3c
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-20 22:08:20 +0600

    login form work is progressing

--- my-project/src/LayOut/AuthLayout.jsx
+++ my-project/src/LayOut/AuthLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
     return (
-        <div>
+        <div className='w-11/12 mx-auto bg-base-200'>
             <Header></Header>
 
             <Outlet></Outlet>
--- my-project/src/Pages/Login.jsx
+++ my-project/src/Pages/Login.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            This is Login
+        <div className='w-4/6 mx-auto bg-[#FFFFFF] *:border'>
+            <h1 className='font-semibold text-center my-5'>Login your account</h1>
+            <div>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Email</legend>
+                    <input type="email" className="input" placeholder="Type here" />
+                </fieldset>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Password</legend>
+                    <input type="Pasword" className="input" placeholder="Type here" />
+     
+                </fieldset>
+            </div>
         </div>
     );
 };
